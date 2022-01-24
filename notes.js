8be0ea906163d9b1d067bb1e17e2eab0bb9710bd
AndreasLonn <45557341+AndreasLonn@users.noreply.github.com>
2022-01-24 15:46:00 +0100

    * Fix bugs * Add ability to remove notes * Add Escape keyboard shortcut to close all panels

--- notes.js
+++ notes.js
@@ -5,37 +5,43 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/Notes/sw.js');
 };
 
+function q(elem, parentElem=document) { return parentElem.querySelector(elem); }
+function qs(elems, parentElem=document) { return parentElem.querySelectorAll(elems); }
+
 class Note {
     title;
     content;
-    constructor(title = "", content = "") {
+    constructor(title = '', content = '') {
         this.title = title;
         this.content = content;
     }
 }
 
 function setVisible(panel, visible) {
+    if(typeof panel === 'string') panel = q(panel);
     panel.parentElement.classList.toggle('visible', visible);
 }
 
+// Yes No Cancel
 function YNC(title, message, onyes, onno, oncancel) {
-    yncpanel.querySelector('h2').innerText = title;
-    yncpanel.querySelector('p').innerText = message;
-    yncpanel.parentElement.onclick = e => {
-        if(e.target.dataset.type === "yes") {
+    let yncPanel = q('#ync-panel');
+    yncPanel.querySelector('h2').textContent = title;
+    yncPanel.querySelector('p').textContent = message;
+    yncPanel.parentElement.onclick = e => {
+        if(e.target.dataset.type === 'yes') {
             if(onyes) onyes();
-            setVisible(yncpanel, false);
+            setVisible(yncPanel, false);
         }
-        if(e.target.dataset.type === "no") {
+        else if(e.target.dataset.type === 'no') {
             if(onno) onno();
-            setVisible(yncpanel, false);
+            setVisible(yncPanel, false);
         }
-        else if(e.target.dataset.type === "cancel" || e.target.classList.contains('panelbackground')) {
+        else if(e.target.dataset.type === 'cancel' || e.target.classList.contains('panel-background')) {
             if(oncancel) oncancel();
-            setVisible(yncpanel, false);
+            setVisible(yncPanel, false);
         }
     };
-    setVisible(yncpanel, true);
+    setVisible(yncPanel, true);
 }
 
 var settings = JSON.parse(localStorage.getItem('notes-settings') || '{"autosave": true, "fontfamily": "font-monospace"}');
@@ -43,7 +49,8 @@ var notes = JSON.parse(localStorage.getItem('notes-notes') || '[{"title": "", "c
 
 Object.defineProperty(settings, 'selected', {
     get: function() {
-        return Number(localStorage.getItem('notes-selected') || 0);
+        let selected = Number(localStorage.getItem('notes-selected'));
+        return notes.length > selected ? selected : 0;
     },
     set: function(newValue) {
         localStorage.setItem('notes-selected', newValue);
@@ -54,25 +61,32 @@ Object.defineProperty(settings, 'selected', {
  * Read the notes from localStorage
  */
 function loadNote() {
-    notetitle.value = notes[settings.selected].title;
-    notecontent.value = notes[settings.selected].content;
-    setVisible(menupanel, false);
+    if(notes.length === 0) notes = [{"title": "", "content": ""}];
+    q('#note-title').value = notes[settings.selected].title;
+    q('#note-content').value = notes[settings.selected].content;
+    setVisible('#menu-panel', false);
 }
 loadNote();
 
 /**
+ * Save notes to localStorage
+ */
+function saveNotesList() {
+    localStorage.setItem('notes-notes', JSON.stringify(notes));
+}
+/**
  * Save note title to localStorage
  */
 function saveNoteTitle() {
-    notes[settings.selected].title = notetitle.value;
-    localStorage.setItem('notes-notes', JSON.stringify(notes));
+    notes[settings.selected].title = q('#note-title').value;
+    saveNotesList();
 }
 /**
  * Save note content to localStorage
  */
 function saveNoteContent() {
-    notes[settings.selected].content = notecontent.value;
-    localStorage.setItem('notes-notes', JSON.stringify(notes));
+    notes[settings.selected].content = q('#note-content').value;
+    saveNotesList();
 }
 /**
  * Save note content to localStorage
@@ -86,19 +100,19 @@ function saveNote() {
  * Save notes to localStorage when notes changed if autosave is enabled
  */
 if (settings.autosave) {
-    notetitle.addEventListener('input', saveNoteTitle);
-    notecontent.addEventListener('input', saveNoteContent);
+    q('#note-title').addEventListener('input', saveNoteTitle);
+    q('#note-content').addEventListener('input', saveNoteContent);
 }
 else
-    savebtn.classList.add('visible');
+    q('#save-btn').classList.add('visible');
 
 if (settings.fontfamily)
-    document.querySelector('body').classList.add(settings.fontfamily);
+    q('body').classList.add(settings.fontfamily);
 
 /**
  * Save notes to localStorage when save button is clicked
  */
-savebtn.addEventListener('click', saveNote);
+q('#save-btn').addEventListener('click', saveNote);
 
 /**
  * Save notes to localStorage when Ctrl + S
@@ -108,32 +122,37 @@ window.addEventListener('keydown', function(e) {
         e.preventDefault();
         saveNote();
     }
+    else if (e.key === 'Escape') {
+        e.preventDefault();
+        qs('div.panel section.panel').forEach(elem => setVisible(elem, false));
+    }
 });
 
-settingsbtn.addEventListener('click', () => {
-    settingspanel.querySelector('form input#settings-autosave').checked = settings.autosave;
-    settingspanel.querySelector('form select#settings-fontfamily').value = settings.fontfamily || "";
-    setVisible(settingspanel, true);
+q('#settings-btn').addEventListener('click', () => {
+    q('#settings-panel form input#settings-autosave').checked = settings.autosave;
+    q('#settings-panel form select#settings-fontfamily').value = settings.fontfamily || '';
+    setVisible('#settings-panel', true);
 });
 
-menubtn.addEventListener('click', () => {
+q('#menu-btn').addEventListener('click', () => {
     let newElems = [];
     notes.forEach((e, i) => {
         let newElem = document.createElement('li');
-        newElem.innerHTML = `<button data-i="${i}">${e.title || "No Title"}</button>`;
+        newElem.innerHTML = `<button data-i="${i}"></button><button aria-label="Delete" title="Delete">X</button>`;
+        q('button', newElem).textContent = e.title || 'No Title';
         newElems.push(newElem);
     });
-    menupanel.querySelector('ul').replaceChildren(...newElems);
-    setVisible(menupanel, true);
+    q('#menu-panel ul').replaceChildren(...newElems);
+    setVisible('#menu-panel', true);
 });
 
-document.querySelectorAll('div.panel div.panelbackground:not(.yncpanel)').forEach(e =>
+qs('div.panel div.panel-background:not(.ync-panel)').forEach(e =>
     e.addEventListener('click', () => setVisible(e, false)));
 
-menupanel.addEventListener('click', e => {
+q('#menu-panel').addEventListener('click', e => {
     if(e.target.dataset.i) {
         if(!settings.autosave)
-            YNC("Do want to save?", "Do you want to save notes before closing?", () => {
+            YNC('Do want to save?', 'Do you want to save notes before closing?', () => {
                 saveNote();
                 settings.selected = Number(e.target.dataset.i);
                 loadNote();
@@ -146,9 +165,21 @@ menupanel.addEventListener('click', e => {
             loadNote();
         }
     }
-    else if(e.target.id === "addnotesbtn"){
+    else if(e.target.parentElement.children[0].dataset.i) {
+        let i = Number(e.target.parentElement.children[0].dataset.i);
+        YNC(`Do you want to delete "${
+                notes[i].title || 'No Title'
+            }"?`, `Are you sure that you want to delete "${
+                notes[i].title || 'No Title'
+            }" (this cannot be undone)?`, () => {
+                notes.splice(i, 1);
+                saveNotesList();
+                loadNote();
+        });
+    }
+    else if(e.target.id === 'add-notes-btn'){
         if(!settings.autosave)
-            YNC("Do want to save?", "Do you want to save notes before closing?", () => {
+            YNC('Do want to save?', 'Do you want to save notes before closing?', () => {
                 saveNote();
                 settings.selected = notes.push(new Note()) - 1;
                 loadNote();
@@ -164,9 +195,12 @@ menupanel.addEventListener('click', e => {
             saveNote();
         }
     }
-    // else if(e.target.id === "editnotesbtn"){
-    //     console.log("Edit");
-    // }
+    else if(e.target.id === 'edit-notes-btn'){
+        q('#menu-panel').classList.add('editing');
+    }
+    else if(e.target.id === 'done-edit-notes-btn'){
+        q('#menu-panel').classList.remove('editing');
+    }
 });
 
 /**
@@ -176,7 +210,7 @@ function saveSettings(e) {
     e.preventDefault();
 
     // Hide settings panel
-    setVisible(settingspanel, false);
+    setVisible('#settings-panel', false);
 
     // Set settings values to the ones specified
     settings.autosave = e.target.querySelector('input#settings-autosave').checked;
@@ -187,19 +221,19 @@ function saveSettings(e) {
 
     // Apply settings
     if (settings.autosave) {
-        notetitle.addEventListener('input', saveNoteTitle);
-        notecontent.addEventListener('input', saveNoteContent);
-        savebtn.classList.remove('visible');
+        q('#note-title').addEventListener('input', saveNoteTitle);
+        q('#note-content').addEventListener('input', saveNoteContent);
+        q('#save-btn').classList.remove('visible');
     } else {
-        notetitle.removeEventListener('input', saveNoteTitle);
-        notecontent.removeEventListener('input', saveNoteContent)
-        savebtn.classList.add('visible');
+        q('#note-title').removeEventListener('input', saveNoteTitle);
+        q('#note-content').removeEventListener('input', saveNoteContent)
+        q('#save-btn').classList.add('visible');
     }
-    document.querySelector('body').classList.remove(...document.querySelector('body').classList);
-    document.querySelector('body').classList.add(settings.fontfamily);
+    q('body').classList.remove(...q('body').classList);
+    q('body').classList.add(settings.fontfamily);
 }
 
-document.querySelector('#toolbar').addEventListener('click', e => {
+q('#toolbar').addEventListener('click', e => {
     e.preventDefault();
-    notecontent.focus();
+    q('#note-content').focus();
 });
